e751d675b3f677fa5f385185fdda1cb93301a41d
Emily Wirtz <ewirtz3@gmail.com>
2021-01-26 21:32:21 -0600

    commenting out footer links so will deploy correctly

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,19 +6,19 @@ export default function Footer() {
       <div className="container d-flex">
         <div>
           <i className="fab fa-instagram fa-lg" aria-labelledby="instagram">
-            <a href="#" alt="link to NSXfitness Instagram"></a>
+            {/* <a href="#" alt="link to NSXfitness Instagram"></a> */}
           </i>
           <span id="instagram">@nsxfitness</span>
         </div>
         <div>
           <i className="fab fa-facebook fa-lg" aria-labelledby="facebook">
-            <a href="#" alt="link to NSXfitness Facebook"></a>
+            {/* <a href="#" alt="link to NSXfitness Facebook"></a> */}
           </i>
           <span id="facebook">NSXfitness</span>
         </div>
         <div>
           <i className="fab fa-tiktok fa-lg" aria-labelledby="tik-tok">
-            <a href="#" alt="link to NSXfitness TikTok"></a>
+            {/* <a href="#" alt="link to NSXfitness TikTok"></a> */}
           </i>
           <span id="tik-tok">@nsxfitness</span>
         </div>
